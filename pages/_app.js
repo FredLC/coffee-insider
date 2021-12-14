0e803d822fd87ccdaa886af38b2db12215d6e1a4
fredlc <fredlefevre@outlook.com>
2021-12-14 16:57:26 -0500

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2021 Fred Lefebvre</p>
+      </footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
