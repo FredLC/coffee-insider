fb7d4333b2974b98e21fd39957d2d9200c0e8c47
fredlc <fredlefevre@outlook.com>
2021-12-15 11:11:00 -0500

    Created and styled card component along with integration of data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
+
 import Banner from "../components/banner";
+import Card from "../components/card";
+
+import coffeeStores from "../data/coffee-stores.json";
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {
@@ -23,6 +28,19 @@ export default function Home() {
         />
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
+        </div>
+
+        <div className={styles.sectionWrapper}>
+          <div className={styles.cardLayout}>
+            {coffeeStores.map((coffeeStore) => (
+              <Card
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}`}
+                className={styles.card}
+              />
+            ))}
+          </div>
         </div>
       </main>
     </div>
