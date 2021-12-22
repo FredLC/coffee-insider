3fee562a2ab201a37ed5366f40e62f328846c29a
fredlc <fredlefevre@outlook.com>
2021-12-22 16:08:52 -0500

    Implementation of pre-rendering on coffee store page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,14 +1,53 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export const getStaticProps = ({ params }) => {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (coffeeStore) => coffeeStore.id.toString() === params.id
+      ),
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+const CoffeeStore = (props) => {
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const { address, name, neighbourhood } = props.coffeeStore;
+
   return (
     <div>
-      {router.query.id}
+      <Head>
+        <title>{name}</title>
+      </Head>
       <Link href="/">
         <a>Back to home</a>
       </Link>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,15 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner";
 import Card from "../components/card";
 
-import coffeeStores from "../data/coffee-stores.json";
+import coffeeStoresData from "../data/coffee-stores.json";
 
-export default function Home() {
+export async function getStaticProps(context) {
+  return {
+    props: { coffeeStores: coffeeStoresData },
+  };
+}
+
+export default function Home(props) {
   const handleOnBannerBtnClick = () => {
     console.log("button clicked");
   };
@@ -31,16 +37,22 @@ export default function Home() {
         </div>
 
         <div className={styles.sectionWrapper}>
-          <div className={styles.cardLayout}>
-            {coffeeStores.map((coffeeStore) => (
-              <Card
-                name={coffeeStore.name}
-                imgUrl={coffeeStore.imgUrl}
-                href={`/coffee-store/${coffeeStore.id}`}
-                className={styles.card}
-              />
-            ))}
-          </div>
+          {props.coffeeStores.length > 0 && (
+            <>
+              <h2 className={styles.heading2}>Toronto Stores</h2>
+              <div className={styles.cardLayout}>
+                {props.coffeeStores.map((coffeeStore) => (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={coffeeStore.imgUrl}
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </main>
     </div>
